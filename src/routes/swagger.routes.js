245eba551678e0bbf2573a4bc1247f38895bf768
Gustavo Mello <mographllo@gmail.com>
2023-06-02 16:03:39 -0300

    removed assert

--- src/routes/swagger.routes.js
+++ src/routes/swagger.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 export const route = express.Router();
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../swagger.json" assert { type: "json" };
+import swaggerDocument from "../swagger.json";
 
 route.use("/", swaggerUi.serve);
 route.get("/", swaggerUi.setup(swaggerDocument));
